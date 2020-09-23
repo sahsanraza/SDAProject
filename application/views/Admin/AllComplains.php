0541fc85ba81af768a1cf876ad0a3a110cc85a04
sahsanraza <ahsnraza36@gmail.com>
2020-09-23 16:10:09 +0500

    Staged conflicts

--- application/views/Admin/AllComplains.php
+++ application/views/Admin/AllComplains.php
@@ -4,79 +4,80 @@
             <?php echo $this->session->flashdata('padded'); ?>
         </div>
     <?php endif; ?>
-   
-    <?php if (!empty($Complains)) : ?>
-        <h2 class="mb-4">Complains</h2>
-        <div class="table-responsive">
-            <table class="table table-hover ">
-                <thead class="bg-dark text-white">
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Subject</th>
-                        <th scope="col">Complain Message</th>
-                        <th scope="col">Response</th>
-                        <th scope="col">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($Complains as $Complain) : ?>
+    <div class="col-md-12">
+        <?php if (!empty($Complains)) : ?>
+            <h2 class="mb-4">Complains</h2>
+            <div class="table-responsive">
+                <table class="table table-hover ">
+                    <thead class="bg-dark text-white">
                         <tr>
-                            <th scope="row" style="width: 50px;"><?php echo $Complain['ComplainID']; ?></th>
-                            <td style="width: 150px;"><?php echo $Complain['Email']; ?></td>
-                            <td><?php echo $Complain['Subject']; ?></td>
-                            <td style="width: 200px;"><?php echo $Complain['Complain']; ?></td>
-                            <?php if ($Complain['Response'] != NULL) : ?>
-                                <td style="width: 300px;"> <?php echo $Complain['Response'] ?></td>
-                            <?php else : ?>
-                                <td style="width: 300px;">
+                            <th scope="col">ID</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Subject</th>
+                            <th scope="col">Complain Message</th>
+                            <th scope="col">Response</th>
+                            <th scope="col">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($Complains as $Complain) : ?>
+                            <tr>
+                                <th scope="row" style="width: 50px;"><?php echo $Complain['ComplainID']; ?></th>
+                                <td style="width: 150px;"><?php echo $Complain['Email']; ?></td>
+                                <td><?php echo $Complain['Subject']; ?></td>
+                                <td style="width: 200px;"><?php echo $Complain['Complain']; ?></td>
+                                <?php if ($Complain['Response'] != NULL) : ?>
+                                    <td style="width: 300px;"> <?php echo $Complain['Response'] ?></td>
+                                <?php else : ?>
+                                    <td style="width: 300px;">
+                                        <?php
+                                        echo form_open('Admin/UpdateResponse/' . $Complain['ComplainID']);
+                                        $opts = array(
+                                            'class' => 'input',
+                                            'placeholder' => 'Add Response here',
+                                            'name' => 'Response'
+                                        );
+                                        echo form_input($opts);
+                                        $data = array(
+                                            'name' => 'SubmitResponse',
+                                            'value' => 'Submit',
+                                            'class' => 'btn btn-sm btn-success'
+                                        );
+                                        echo form_submit($data);
+                                        echo form_close();
+                                        ?>
+                                    </td>
+                                <?php endif; ?>
+
+                                <td>
                                     <?php
-                                    echo form_open('Admin/UpdateResponse/' . $Complain['ComplainID']);
-                                    $opts = array(
-                                        'class' => 'input',
-                                        'placeholder' => 'Add Response here',
-                                        'name' => 'Response'
-                                    );
-                                    echo form_input($opts);
+                                    echo form_open('Admin/UpdateComplainStatus/' . $Complain['ComplainID']);
+                                    $opts = [
+                                        'Pending'  => 'Pending',
+                                        'Received'    => 'Received',
+                                        'Resolved' => 'Resolved'
+                                    ];
+                                    echo form_dropdown('Status', $opts, $Complain['Status']);
                                     $data = array(
-                                        'name' => 'SubmitResponse',
-                                        'value' => 'Submit',
-                                        'class' => 'btn btn-sm btn-success'
+                                        'name' => 'UpdateStatus',
+                                        'value' => 'Change',
+                                        'id' => 'StatusBtn',
+                                        'class' => 'btn btn-sm btn-secondary'
                                     );
                                     echo form_submit($data);
                                     echo form_close();
                                     ?>
                                 </td>
-                            <?php endif; ?>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        <?php else : ?>
+            <div class="col-md-4 offset-4 text-center">
+                <h1>No Complains found! </h1>
+            </div>
 
-                            <td>
-                                <?php
-                                echo form_open('Admin/UpdateComplainStatus/' . $Complain['ComplainID']);
-                                $opts = [
-                                    'Pending'  => 'Pending',
-                                    'Received'    => 'Received',
-                                    'Resolved' => 'Resolved'
-                                ];
-                                echo form_dropdown('Status', $opts, $Complain['Status']);
-                                $data = array(
-                                    'name' => 'UpdateStatus',
-                                    'value' => 'Change',
-                                    'id' => 'StatusBtn',
-                                    'class' => 'btn btn-sm btn-secondary'
-                                );
-                                echo form_submit($data);
-                                echo form_close();
-                                ?>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
-    <?php else : ?>
-        <div class="col-md-4 offset-4 text-center">
-            <h1>No Complains found! </h1>
-        </div>
-
-    <?php endif; ?>
+        <?php endif; ?>
+    </div>
 </div>
--- application/views/Admin/AllOrders.php
+++ application/views/Admin/AllOrders.php
@@ -1,61 +1,63 @@
 <div class="row" style="padding:1% 5%">
-<?php if ($this->session->flashdata('padded')) : ?>
+    <?php if ($this->session->flashdata('padded')) : ?>
         <div class="alert alert-success" role="alert">
             <?php echo $this->session->flashdata('padded'); ?>
         </div>
     <?php endif; ?>
-    <h2 class="mb-4">Orders</h2>
-  
-    <?php if (!empty($Orders)) : ?>
-        <div class="table-responsive">
-            <table class="table table-hover ">
-                <thead class="bg-dark text-white">
-                    <tr>
-                        <th scope="col">Order #</th>
-                        <th scope="col">Customer Name</th>
-                        <th scope="col">Created Date</th>
-                        <th scope="col">Total</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Detail</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($Orders as $Order) : ?>
+    <div class="col-md-12">
+        <h2 class="mb-4">Orders</h2>
+
+        <?php if (!empty($Orders)) : ?>
+            <div class="table-responsive">
+                <table class="table table-hover ">
+                    <thead class="bg-dark text-white">
                         <tr>
-                            <th scope="row"><?php echo $Order['OrderID']; ?></th>
-                            <td><?php echo $Order['FullName']; ?></td>
-                            <td><?php echo date("d M Y h:i A", strtotime($Order['DateCreated'])); ?></td>
-                            <td>Rs. <?php echo number_format($Order['TotalPrice'], 0); ?></td>
-                            <td>
-                                <?php
+                            <th scope="col">Order #</th>
+                            <th scope="col">Customer Name</th>
+                            <th scope="col">Created Date</th>
+                            <th scope="col">Total</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Detail</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($Orders as $Order) : ?>
+                            <tr>
+                                <th scope="row"><?php echo $Order['OrderID']; ?></th>
+                                <td><?php echo $Order['FullName']; ?></td>
+                                <td><?php echo date("d M Y h:i A", strtotime($Order['DateCreated'])); ?></td>
+                                <td>Rs. <?php echo number_format($Order['TotalPrice'], 0); ?></td>
+                                <td>
+                                    <?php
                                     echo form_open('Admin/UpdateOrderStatus/' . $Order['OrderID']);
                                     $options = [
-                                    'Pending'  => 'Pending',
-                                    'Completed'    => 'Completed',
-                                    'Cancelled'  => 'Cancelled',
-                                ];
-                                echo form_dropdown('Status', $options, $Order['Status']);
-                                $data = array(
-                                    'name' => 'UpdateStatus',
-                                    'value' => 'Change',
-                                    'class' => 'btn btn-sm btn-secondary'
-                                );
-                                echo form_submit($data);
-                                echo form_close();
-                                ?>
-                            </td>
-                            <td><a class="btn btn-sm btn-primary" href="<?php echo site_url('Admin/OrderDetail/' . $Order['OrderID']); ?>">View Order</a></td>
-                        </tr>
-                    <?php endforeach; ?>
+                                        'Pending'  => 'Pending',
+                                        'Completed'    => 'Completed',
+                                        'Cancelled'  => 'Cancelled',
+                                    ];
+                                    echo form_dropdown('Status', $options, $Order['Status']);
+                                    $data = array(
+                                        'name' => 'UpdateStatus',
+                                        'value' => 'Change',
+                                        'class' => 'btn btn-sm btn-secondary'
+                                    );
+                                    echo form_submit($data);
+                                    echo form_close();
+                                    ?>
+                                </td>
+                                <td><a class="btn btn-sm btn-primary" href="<?php echo site_url('Admin/OrderDetail/' . $Order['OrderID']); ?>">View Order</a></td>
+                            </tr>
+                        <?php endforeach; ?>
 
 
-                </tbody>
-            </table>
-        </div>
-    <?php else : ?>
-        <div class="col-md-4 offset-4 text-center">
-            <h1>No Orders found! </h1>
-        </div>
+                    </tbody>
+                </table>
+            </div>
+        <?php else : ?>
+            <div class="col-md-4 offset-4 text-center">
+                <h1>No Orders found! </h1>
+            </div>
 
-    <?php endif; ?>
+        <?php endif; ?>
+    </div>
 </div>
